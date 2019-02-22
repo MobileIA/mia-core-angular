27ce3d8940a684e515e8e18711531c331c8f875f
Matias Camiletti <matias.camiletti@gmail.com>
2019-02-22 18:32:58 -0300

    Funcionalidad para subir archivo con progreso.

--- projects/mobileia/core/src/lib/mia-file.service.ts
+++ projects/mobileia/core/src/lib/mia-file.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,12 +9,25 @@ export class MiaFileService {
 
   constructor(private http: HttpClient) { }
 
-  public upload(app_id : string, files: FileList) : Observable<any>{
+  public upload(app_id : string, files: FileList): Observable<any> {
     var formData: FormData = new FormData();
     formData.append('app_id', app_id);
     for(var i = 0; i < files.length; i++){
       formData.append('file['+i+']', files[i]);
     }
     return this.http.post<any>('https://files.mobileia.com/api/upload', formData);
+  }
+
+  public uploadWithProgress(app_id : string, files: FileList): Observable<any> {
+    var formData: FormData = new FormData();
+    formData.append('app_id', app_id);
+    for(var i = 0; i < files.length; i++){
+      formData.append('file['+i+']', files[i]);
+    }
+
+    const req = new HttpRequest('POST', 'https://files.mobileia.com/api/upload', formData, {
+      reportProgress: true,
+    });
+    return this.http.request(req);
   }
 }
